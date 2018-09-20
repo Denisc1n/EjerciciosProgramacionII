e9b934cff93f50d737355d7957d451754853becc
alumno <alumno>
2018-09-19 21:36:27 -0300

    Ejercicio33

---
+++ EjerciciosProgramacionII/Ejercicio33/Libro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio33
+{
+    class Libro
+    {
+        private List<string> paginas = new List<string>();
+
+        public string this[int i]
+        {
+            get {
+                if (i > 0 && i < this.paginas.Count)
+                    return paginas[i];
+                else
+                    return $"Pagina {i} No encontrada.";
+                }
+            set
+            {
+                if (i> 0 && i< paginas.Count)
+                {
+                    paginas[i] = value;
+                }
+                else
+                {
+                    paginas.Add(value);
+                }
+            }
+        }
+
+    }
+}
---
+++ EjerciciosProgramacionII/Ejercicio33/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro = new Libro();
+            libro[0] = "Pagina con Datos";
+            libro[1] = "Pagina con Datos en Indice 1";
+            libro[2] = "Pagina con datos en Indice 2";
+
+            Console.WriteLine("Resultado: " + libro[3]);
+            libro[3] = "Pagina con datos en Indice 3";
+            Console.WriteLine("Resultado: " + libro[3]);
+            Console.ReadLine();
+
+
+        }
+    }
+}
