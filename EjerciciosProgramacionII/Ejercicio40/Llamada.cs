70cba7516e15b4f53947f6cd742aeb0f60db67f1
alumno <alumno>
2018-09-26 22:06:35 -0300

    Ejercicio40

---
+++ EjerciciosProgramacionII/Ejercicio40/Llamada.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio37
+{
+    public abstract class Llamada
+    {
+        protected float duracion;
+        protected string numOrigen;
+        protected string numDestino;
+
+        public enum TipoLlamada
+        {
+            Local,
+            Provincial,
+            Todas
+        }
+
+        #region Properties
+
+        public float GetDuracion
+        {
+            get
+            {
+                return this.duracion;
+            }
+        }
+
+        public string GetNumDestino
+        {
+            get
+            {
+                return this.numDestino;
+            }
+        }
+
+        public string GetNumOrigen
+        {
+            get
+            {
+                return this.numOrigen;
+            }
+        }
+
+        public abstract float CostoLlamada
+        {
+            get;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public Llamada( float duracion, string origen, string destino)
+        {
+            this.duracion = duracion;
+            this.numOrigen = origen;
+            this.numDestino = destino;
+        }
+
+        protected virtual string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Datos de Llamada: ");
+            sb.Append($"Numero Origen:{this.numOrigen} - Numero Destino:{this.numDestino} - Duracion:{this.duracion}");
+
+            return sb.ToString();
+        }
+
+        public static int OrdenarPorDuracion(Llamada llamada1, Llamada llamada2)
+        {
+            return llamada1.GetDuracion.CompareTo(llamada2.GetDuracion);
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public static bool operator ==( Llamada llamadaUno, Llamada llamadaDos)
+        {
+            if ( llamadaUno.Equals(llamadaDos) && llamadaUno.numDestino == llamadaDos.numDestino && llamadaUno.numOrigen == llamadaDos.numOrigen )
+                return true;
+
+            return false;
+     
+        }
+
+        public static bool operator !=(Llamada llamadaUno, Llamada llamadaDos)
+        {
+            return !(llamadaUno == llamadaDos);
+        }
+
+        #endregion
+    }
+}
---
+++ EjerciciosProgramacionII/Ejercicio40/Local.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio37
+{
+    public class Local:Llamada
+    {
+        
+        protected float costo;
+        
+        public override float CostoLlamada
+        {
+            get
+            {
+                return this.CalcularCosto();
+            }
+        }
+
+        
+
+        public Local(Llamada llamada, float costo)
+            : base(llamada.GetDuracion, llamada.GetNumDestino, llamada.GetNumOrigen)
+        {
+            this.costo = costo;
+        }
+
+        public Local(string origen, float duracion, string destino, float costo)
+            : base(duracion, destino, origen)  
+        {
+            this.costo = costo;
+        }
+       
+        protected override string Mostrar()
+        {
+            StringBuilder s = new StringBuilder();
+            s.AppendLine(base.ToString() + $"Costo:{this.CostoLlamada}");
+
+            return s.ToString();
+        }
+
+        private float CalcularCosto()
+        {
+            return this.GetDuracion * this.costo;
+        }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Local ? true : false;
+        }
+    }
+}
