2f8efd7acfb089fd7a0b5c6c8332de8e7ae4fce2
alumno <alumno>
2018-10-17 22:01:50 -0300

    Mas Ejercicios

---
+++ EjerciciosProgramacionII/UnitTestEjercicio42/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestEjercicio42")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("UnitTestEjercicio42")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dbcecdc0-7ca0-4ab5-a099-4718a5a231f1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EjerciciosProgramacionII/UnitTestEjercicio42/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ejercicio42;
+
+namespace UnitTestEjercicio42
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void MetodoExceptionTest()
+        {
+            //arrange
+            int numTest1 = 1;
+            int numTest2 = 0;
+
+            ClaseException.MetodoException(numTest1, numTest2);
+            Assert.That.Equals( new object() );
+        }
+
+        
+    }
+}
