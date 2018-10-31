dbf277dfe5f9d48aebd3995a19f6ebda49b5bca0
alumno <alumno>
2018-10-31 19:25:57 -0300

    Ejercicio57

---
+++ EjerciciosProgramacionII/Ejercicio57/Persona.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Ejercicio57
+{
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+
+        public Persona() { }
+
+        public Persona( string inputNombre, string inputApellido)
+        {
+            this.nombre = inputNombre;
+            this.apellido = inputApellido;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return this.apellido;
+            }
+        }
+
+        public static void Guardar( Persona persona)
+        {
+           
+            XmlTextWriter xmlWriter = new XmlTextWriter(ConfigurationManager.AppSettings["RutaArchivo"], Encoding.UTF8);
+            XmlSerializer serializer = new XmlSerializer(typeof(Persona));
+            xmlWriter.Close();
+
+        }
+
+        public static Persona Leer (string ruta)
+        {
+            XmlTextReader xmlReader = new XmlTextReader(ruta);
+            XmlSerializer serializer = new XmlSerializer(typeof(Persona));
+            Persona personaAux;
+            personaAux = (Persona)serializer.Deserialize(xmlReader);
+            xmlReader.Close();
+            return personaAux;
+        }
+    }
+}
---
+++ EjerciciosProgramacionII/Ejercicio57/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona("Denis", "Pedemonte");
+            Console.WriteLine($"Guardando:{persona.Nombre} {persona.Apellido} ");
+            Persona.Guardar(persona);
+            Console.WriteLine($"Guardado Correcto, Procediendo a Leer...");
+
+
+            Persona nuevaPersona = Persona.Leer(Environment.CurrentDirectory + "\\MiArchivo.xml");
+
+            Console.WriteLine($"Persona Leida:{persona.Nombre} {persona.Apellido} ");
+            Console.ReadLine();
+        }
+    }
+}
