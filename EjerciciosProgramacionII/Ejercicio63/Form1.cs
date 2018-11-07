ab5479bada1284728ea8a875905c6a360b7a7a14
alumno <alumno>
2018-11-07 20:23:20 -0300

    Cambios al 63

--- EjerciciosProgramacionII/Ejercicio63/Form1.cs
+++ EjerciciosProgramacionII/Ejercicio63/Form1.cs
@@ -12,6 +12,7 @@ namespace Ejercicio63
 {
     public partial class Form1 : Form
     {
+        Thread miThread;
         public Form1()
         {
             InitializeComponent();
@@ -20,16 +21,39 @@ namespace Ejercicio63
         private void Form1_Load(object sender, EventArgs e)
         {
             AsignarHora(sender, e);
-            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
-            timer.Interval = (1 * 1000);
-            timer.Tick += new EventHandler(AsignarHora);
-            timer.Start();
+            //System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            //timer.Interval = (1 * 1000);
+            //timer.Tick += new EventHandler(AsignarHora);
+            //timer.Start();
+            miThread = new Thread(AsignarHoraThread);
+            miThread.Start();
         }
 
    
         public void AsignarHora( object  args, EventArgs e )
         {
-           this.lblHora.Text =  DateTime.Now.ToString("dd-MM-yyyy hh:mm:ss");
+           this.lblHora.Text = DateTime.Now.ToString("dd-MM-yyyy hh:mm:ss");
+        }
+
+        public void AsignarHoraThread()
+        {
+            while (true)
+            {
+                if (this.lblHora.InvokeRequired)
+                {
+                    this.lblHora.BeginInvoke((MethodInvoker)delegate ()
+                    {
+                        this.lblHora.Text = DateTime.Now.ToString("dd-MM-yyyy hh:mm:ss");
+                    });
+                }
+                Thread.Sleep(500);
+            }
+           
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            miThread.Abort();
         }
     }
 }
