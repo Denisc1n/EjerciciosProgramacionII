1e33d3bed43d6065679264bc8f759df0086ea6ac
alumno <alumno>
2018-11-07 21:28:01 -0300

    Ejercicio64

---
+++ EjerciciosProgramacionII/Ejercicio64/Caja.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejercicio64
+{
+    class Caja
+    {
+        List<string> filaClientes;
+
+        public List<string> GetClientes
+        {
+            get
+            {
+                return this.filaClientes;
+            }
+        }
+
+        public Caja()
+        {
+            this.filaClientes = new List<string>();
+        }
+
+        public void AtenderClientes()
+        {
+            foreach (var item in this.GetClientes)
+            {
+                Console.WriteLine("Atendiendo Cliente " + item );
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ EjerciciosProgramacionII/Ejercicio64/Negocio.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio64
+{
+    class Negocio
+    {
+        Caja cajaUno;
+        Caja cajaDos;
+
+        List<string> clientes;
+
+
+        public Caja GetCajaUno
+        {
+            get
+            {
+                return this.cajaUno;
+            }
+        }
+
+        public Caja GetCajaDos
+        {
+            get
+            {
+                return this.cajaDos;
+            }
+        }
+
+        public List<string> GetClientes
+        {
+            get
+            {
+                return this.clientes;
+            }
+        }
+
+
+        public Negocio(Caja cajaPrimera, Caja cajaSegunda)
+        {
+            this.cajaUno = cajaPrimera;
+            this.cajaDos = cajaSegunda;
+        }
+
+        public void AsignarClientes()
+        {
+            Console.WriteLine("Asignando Clientes...");
+
+            foreach (var item in this.clientes)
+            {
+                if (cajaUno.GetClientes.Count > cajaDos.GetClientes.Count)
+                    cajaDos.GetClientes.Add(item);
+
+                else
+                    cajaUno.GetClientes.Add(item);
+            }
+        }
+
+    }
+}
